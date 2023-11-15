2bc3465951353138993a8b448269adcc905fd3b4
LuminiferousGiraffe <andreas.quist99@gmail.com>
2023-11-15 14:31:44 -0500

    Fixed prompts for creating employees and completed code for updating employees

--- index.js
+++ index.js
@@ -35,12 +35,12 @@ const newRoleQs = [
   {
     type: 'input',
     name: 'dept',
-    message: 'Please enter the id number for the department this role belongs to.'
+    message: 'Please enter the name of the department this role belongs to.'
   },
   {
     type: 'input',
     name: 'salary',
-    message: 'Please enter the salary for this role.'
+    message: 'Please enter the salary for this role (in thousands of dollars per year).'
   }
 ];
 
@@ -59,13 +59,13 @@ const newEmployeeQs = [
   },
   {
     type: 'input',
-    name: 'department',
-    message: "Please enter which department the employee belongs to."
+    name: 'role',
+    message:"Please enter the employee's job title."
   },
   {
     type: 'input',
-    name: 'role',
-    message:"Please enter the employee's role."
+    name: 'manager',
+    message: "Please enter the name of the employee's manager."
   }
 ];
 
@@ -98,13 +98,15 @@ function actionHandler (res){
     showTable('employees');
   }else if(res.action==='Add a department'){
     inquirer.prompt(newDeptQs)
-    .then((res)=>{addToTable('departments','dept_name',/* `"${res.name}"` */ [res.name])});
+    .then((res)=>{addToTable('departments', [res.name])});
   }else if(res.action==='Add a role'){
     inquirer.prompt(newRoleQs)
-    .then((res)=>{addToTable('roles','title, dept_id, salary',/* `"${res.title}","${res.dept}","${res.salary}"` */ [res.title,res.dept,res.salary])});
+    .then((res)=>{addToTable('roles', [res.title,res.dept,res.salary])});
   }else if(res.action==='Add an employee'){
     inquirer.prompt(newEmployeeQs)
-    .then((res)=>{addToTable('employees','first_name, last_name, role_id',/* `"${res.first_name}","${res.last_name}","${res.role}"` */ [res.first_name,res.last_name,res.department,res.role])});
+    .then((res)=>{addToTable('employees',[res.first_name,res.last_name,res.role,res.manager])});
+  } else if (res.action === 'Update an employee role'){
+    updateEmployeeHandler();
   } else if (res.action ==='Quit'){
     process.exit(0);
   } else {
@@ -112,7 +114,43 @@ function actionHandler (res){
   }
 }
 
-async function addToTable(table, rowFormat, rowContent){
+async function addToTable(table, rowContent){
+  if(table==='departments'){
+    db.query(`INSERT INTO departments (dept_name) VALUES ("${rowContent[0]}")`,(err,res)=>{
+      if(err){console.log(err)}
+      else {console.log('Successfully created new department!')}
+      actionPrompt();
+    });
+  } else if(table==='roles'){
+    db.query(`SELECT dept_id FROM departments WHERE dept_name="${rowContent[1]}"`,(err,res)=>{
+      if(err){console.log(err)}
+      else{
+        console.log(res);
+        db.query(
+        `INSERT INTO roles (title, dept_id, salary) VALUES ("${rowContent[0]}", "${res[0].dept_id}", "${rowContent[2]}")`,
+        (err,res) => {
+          if(err){console.log(err)}
+          else {console.log('Successfully created new role!')}
+          actionPrompt();
+        }
+      );}
+    });
+  } else if (table === 'employees'){
+      db.query(`SELECT role_id FROM roles WHERE title = "${rowContent[2]}"`,
+      (err,res)=>{
+        if(err){console.log(err)}
+        else {db.query(`INSERT INTO employees (first_name, last_name, role_id, manager) VALUES 
+        ("${rowContent[0]}","${rowContent[1]}","${res[0].role_id}","${rowContent[3]}")`,
+        (err,res)=>{
+          if(err){console.log(err)}
+          else {
+            console.log('Successfully created new employee profile!');
+          }
+          actionPrompt();
+        })}
+      });
+  }
+/* 
   if(table==='employees'){
     console.log(rowContent);
     var deptID = -1;
@@ -132,7 +170,7 @@ async function addToTable(table, rowFormat, rowContent){
         console.log(res);
       }
     );
-  }
+  } */
 }
 
 /* async function addToTable(table,rowFormat,rowContent){
@@ -146,26 +184,75 @@ async function addToTable(table, rowFormat, rowContent){
 
 async function showTable(table){
   if(table === 'employees'){
-    db.query(`SELECT employees.employee_id, employees.first_name, employees.last_name, roles.title, departments.dept_name, roles.salary
+    db.query(`SELECT employees.employee_id, employees.first_name, employees.last_name, roles.title, departments.dept_name, roles.salary, employees.manager
       FROM employees
       INNER JOIN roles
       ON roles.role_id = employees.role_id
       INNER JOIN departments
       ON roles.dept_id = departments.dept_id`,
-      (err,res)=>{
-        if(err){console.log(err);}
-        console.table(res, ["Employee ID","First Name","Last Name","Role","Department","Salary"]);
-        actionPrompt();
-    });
-  } else {
-      db.query(
-        `SELECT * FROM ${table}`, (err,tb)=>{
-          if(err){console.log(err);}
-          console.table(tb);
-          actionPrompt();
-        },
-      );
+      tableHandler  
+    );
+  } else if (table === 'roles'){
+    db.query(
+      `SELECT roles.role_id, roles.title, departments.dept_name, roles.salary
+      FROM roles
+      INNER JOIN departments
+      ON roles.dept_id = departments.dept_id`, 
+      tableHandler
+    );
+  } else {  
+    db.query(
+      `SELECT * FROM ${table}`, 
+      tableHandler
+    );
   }
+}
+
+function tableHandler(err,tb){
+  if(err){console.log(err)}
+  else {console.table(tb)}
+  actionPrompt();
+}
+
+async function updateEmployeeHandler(){
+  db.query(`SELECT first_name, last_name FROM employees`,
+  (err,res)=>{
+    if(err){console.log(err)}
+    else{
+      const employeesArr = [];
+      for(i=0;i<res.length;i++){
+        var employeeName = res[i].first_name + " " + res[i].last_name;
+        employeesArr.push(employeeName);
+      }
+      console.log(employeesArr);
+      inquirer.prompt([{
+        type: 'list',
+        message: 'Please choose an employee.',
+        choices: employeesArr,
+        name: 'employee'
+      },{
+        type: 'input',
+        message: "Please input the employee's new role.",
+        name: 'role'
+      }]).then((res)=>{
+        db.query(`SELECT role_id FROM roles WHERE title = "${res.role}"`,
+        (err,roleId)=>{
+          if(err){console.log(err)}
+          else{
+            console.log(employeesArr.indexOf(res.employee)+1);
+            db.query(`UPDATE employees SET role_id=${roleId[0].role_id} WHERE employee_id = ${employeesArr.indexOf(res.employee)+1}`,
+            (err,res)=>{
+              if(err){console.log(err)}
+              else{
+                console.log('Successfully updated employee role!');
+                actionPrompt();
+              }
+            });
+          }
+        });
+      });
+    }
+  });
 }
 
 init();
